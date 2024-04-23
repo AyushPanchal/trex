e775594bf5318d083c78c13120e93b51f964c6ce
Ayush <ayush18november@gmail.com>
2024-04-23 10:45:56 +0530

    Constant Color Added!

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:trex/utils/theme/theme.dart';
+import 'app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
