d101bab77b41162ef0eda55b1861f12dfdaf2b31
Ayush <ayush18november@gmail.com>
2024-04-26 20:44:50 +0530

    created reset password and forget password pages, and here our UI section for authentications ends

---
+++ lib/features/authentication/screens/password_configuration/forget_password.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/screens/password_configuration/reset_password.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: Icon(
+            Iconsax.arrow_left_2,
+            color: isDarkMode ? Colors.grey : TColors.primary,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Headings
+            Text(
+              TTexts.forgetPasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwItems,
+            ),
+            Text(
+              TTexts.forgetPasswordSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections * 2,
+            ),
+
+            //Text Field
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: TTexts.email,
+                prefixIcon: Icon(Iconsax.direct_right),
+              ),
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections,
+            ),
+
+            //Submit button
+            SizedBox(
+              width: double.maxFinite,
+              child: ElevatedButton(
+                onPressed: () => Get.off(
+                  () => const ResetPassword(),
+                ),
+                child: const Text(
+                  TTexts.submit,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
