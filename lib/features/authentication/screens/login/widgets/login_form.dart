c2880846e416aab2ac9b6b8c76ad5c7299b87352
Ayush <ayush18november@gmail.com>
2024-04-27 12:07:50 +0530

    Navigation menu setup for shop

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:t_store/features/authentication/screens/signup/signup.dart';
+import 'package:t_store/navigation_menu.dart';
 
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
@@ -67,7 +68,9 @@ class LoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(
+                  () => const NavigationMenu(),
+                ),
                 child: const Text(TTexts.signIn),
               ),
             ),
---
+++ lib/navigation_menu.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/shop/screens/home/home.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = THelperFunctions.isDarkMode(context);
+    final controller = Get.put(NavigationController());
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+            elevation: 0,
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (index) =>
+                controller.selectedIndex.value = index,
+            destinations: const [
+              NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
+              NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
+              NavigationDestination(
+                  icon: Icon(Iconsax.heart), label: "Wishlist"),
+              NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
+            ],
+            backgroundColor: isDarkMode ? TColors.black : Colors.white,
+            indicatorColor: isDarkMode
+                ? TColors.white.withOpacity(0.1)
+                : TColors.black.withOpacity(0.1)),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    HomeScreen(),
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.pink,
+    ),
+    Container(
+      color: Colors.purple,
+    ),
+  ];
+}
